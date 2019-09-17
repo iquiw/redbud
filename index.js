1849b3e7a168cd8a4a9d2adf091ae7fe59f87a84
Iku Iwasa <iku.iwasa@gmail.com>
2019-09-17 22:23:01 +0900

    redbud initial import

---
+++ index.js
@@ -0,0 +1,69 @@
+const _ = require('koa-route');
+const koa = require('koa');
+const views = require('koa-views');
+const send = require('koa-send');
+const moment = require('moment');
+
+const Redmine = require('./lib/redmine');
+
+const PATH_PREFIX = process.env['REDBUD_PATH_PREFIX'] || '';
+const PORT = process.env['REDBUD_PORT'] || '3000';
+
+function isValidVersion(s) {
+  return s && /^\d+$/.test(s);
+}
+
+function isValidDate(s) {
+  return s && /^\d{4}-\d{2}-\d{2}$/.test(s);
+}
+
+async function index(ctx) {
+  const redmine = new Redmine(process.env['REDMINE_BASE_URL'], process.env['REDMINE_API_KEY']);
+
+  let {
+    versionId,
+    startDate,
+    endDate,
+    dueDate,
+  } = ctx.request.query;
+
+  if (!isValidVersion(versionId)) {
+    ctx.body = {
+      error: 'Please specify version number as "versionId".'
+    };
+    return;
+  }
+
+  if (!isValidDate(startDate)) {
+    ctx.body = {
+      error: 'Please specify "startDate" in YYYY-MM-DD format.'
+    };
+    return;
+  }
+
+  let vis = await redmine.getVersionIssues(versionId);
+  vis.startDate = startDate;
+  vis.endDate = endDate || moment().subtract(1, 'days').format('YYYY-MM-DD');
+  vis.dueDate = dueDate || vis.dueDate;
+
+  if (!isValidDate(vis.dueDate)) {
+    ctx.body = {
+      error: 'Please specify "dueDate" in the version or as query string in YYYY-MM-DD format.'
+    };
+    return;
+  }
+
+  await ctx.render('index.html', {
+    issues: JSON.stringify(vis),
+    prefix: PATH_PREFIX
+  });
+}
+
+const app = new koa();
+app
+  .use(views('views', { map: { html: 'nunjucks' } }))
+  .use(_.get(PATH_PREFIX + '/', index))
+  .use(_.get(PATH_PREFIX + '/dist/index.js', async (ctx) => {
+    await send(ctx, 'dist/index.js');
+  }));
+app.listen(PORT);
